4b4f15a6e1514427e6a9cdf227d0edcfcbea897e
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-06-14 20:39:27 +0800

    point

--- mike_codez/src/main.zig
+++ mike_codez/src/main.zig
@@ -31,14 +31,14 @@ pub fn main() !void {
     const g1 = [_][]const u8{ "Apple", "Banana", "Cherry" };
     try stdOut.print("\ng1: type = {}\n", .{@TypeOf(g1)});
     try stdOut.print("g1: len = {}\n", .{g1.len});
-    try stdOut.print("g1 = {s}\n", .{g1});
+    try stdOut.print("g1: content = {s}\n", .{g1});
 
     const vec1 = @Vector(6, u32){ 10, 13, 17, 22, 28, 35 };
     const vec2 = @Vector(6, u32){ 1, 3, 5, 7, 9, 11 };
     const vec3 = vec1 + vec2; // value will be added, same type as themselves
     try stdOut.print("\nvec3: type = {}\n", .{@TypeOf(vec3)});
     try stdOut.print("vec3: len = {}\n", .{@typeInfo(@TypeOf(vec3)).vector.len});
-    try stdOut.print("vec3 = {any}\n", .{vec3});
+    try stdOut.print("vec3: content = {any}\n", .{vec3});
     // if add @Vector(6, u32) with @Vector(5, u32), it will get an error
     //      const vec3 = vec1 + vec2;
     //                   ~~~~~^~~~~~ (error: vector length mismatch)
@@ -47,6 +47,11 @@ pub fn main() !void {
     //      const vec3 = vec1 + vec2;
     //                          ^~~~ length 5 here
 
+    var value1: u32 = 10;
+    const point1: *u32 = &value1;
+    point1.* = 50;
+    try stdOut.print("\npoint1 = {d}\nvalue1 = {d}\n", .{ point1, value1 });
+
     const final_elapsed_time_ns: i128 = std.time.nanoTimestamp() - start_time; // i128除法需要配置
-    std.debug.print("\nExec Done wiht --> {}ns\n", .{final_elapsed_time_ns});
+    std.debug.print("\nExec Done with --> {}ns\n", .{final_elapsed_time_ns});
 }
