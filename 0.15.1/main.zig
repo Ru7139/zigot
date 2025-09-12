d9f799ed2dcdf49058cf8ae794df893211ddb13e
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-09-12 23:20:08 +0900

    remove no meaning code
    for trying to enable test in main

--- 0.15.1/main.zig
+++ 0.15.1/main.zig
@@ -4,12 +4,4 @@ const test_file = @import("test_zh_doc.zig");
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-pub fn main() !void {
-    const program_start = std.time.nanoTimestamp();
-    std.testing.refAllDeclsRecursive(test_file);
-
-    // std.Thread.sleep(1_000_000_000);
-    const program_end = std.time.nanoTimestamp();
-    const diff_start_end_time = program_end - program_start;
-    print("task consumed ---> {}", .{diff_start_end_time});
-}
+pub fn main() !void {}
