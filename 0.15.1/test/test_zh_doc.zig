2ab12606d6124b15a7cd6bd62b236f06dfa626cf
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-09-18 14:24:40 +0900

    Update

--- 0.15.1/test/test_zh_doc.zig
+++ 0.15.1/test/test_zh_doc.zig
@@ -2,8 +2,15 @@ const std = @import("std");
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-fn println_fn(name: []const u8, x: anytype) void {
-    print("{s}: {} ---> {}\n", .{ name, @TypeOf(x), x });
+const ANY_PRINTLN: bool = true;
+const NORMAL_PRINTLN: bool = false;
+
+fn println_fn(name: []const u8, x: anytype, comptime use_any: bool) void {
+    if (use_any) {
+        print("{s}: {} ---> {any}\n", .{ name, @TypeOf(x), x });
+    } else {
+        print("{s}: {} ---> {}\n", .{ name, @TypeOf(x), x });
+    }
 }
 
 test "test_or_skip_example" {
@@ -32,22 +39,22 @@ test "1.1 ---> variable declare" {
     assert(p == 100);
 
     const t: u32 = undefined;
-    println_fn("t(undefined)", t);
+    println_fn("t(undefined)", t, NORMAL_PRINTLN);
 
     var x: u32 = undefined;
     var y: u32 = undefined;
     var z: u32 = undefined;
     const tuple_1 = .{ 1, 2, 3 };
     x, y, z = tuple_1;
-    println_fn("tuple ---> x+y+z", x + y + z);
+    println_fn("tuple ---> x+y+z", x + y + z, NORMAL_PRINTLN);
 
     const array_1 = [_]u32{ 7, 8, 9 };
     x, y, z = array_1;
-    println_fn("array ---> x+y+z", x + y + z);
+    println_fn("array ---> x+y+z", x + y + z, NORMAL_PRINTLN);
 
     const vec_1: @Vector(3, u8) = .{ 1, 0, 1 };
     x, y, z = vec_1;
-    println_fn("vector ---> x+y+z", x + y + z);
+    println_fn("vector ---> x+y+z", x + y + z, NORMAL_PRINTLN);
 
     {
         var q: i32 = 1;
@@ -61,8 +68,8 @@ test "1.1 ---> variable declare" {
             break :block_0;
         };
         add_u;
-        println_fn("u", u);
-        println_fn("closure ---> add_u", add_u);
+        println_fn("u", u, NORMAL_PRINTLN);
+        println_fn("closure ---> add_u", add_u, NORMAL_PRINTLN);
     }
 
     // --- forbidden --- (shadow)
@@ -77,12 +84,12 @@ test "1.2 @(1/3) ---> basic types (numbers value)" {
     const permisssions = 0o7_5_5;
     const big_address = 0xFF80_FFFF_FFFF_FFFF;
     const tuple_1 = .{ one_billion, binary_mask, permisssions, big_address };
-    println_fn("tuple_1", tuple_1);
+    println_fn("tuple_1", tuple_1, NORMAL_PRINTLN);
 
     const int_7: i7 = std.math.maxInt(i7);
     const uint_7: u7 = std.math.maxInt(u7);
     const sum_i7_u7: u32 = @as(u32, @intCast(int_7)) + @as(u32, uint_7);
-    println_fn("maxInt.i7 + maxInt.u7", sum_i7_u7);
+    println_fn("maxInt.i7 + maxInt.u7", sum_i7_u7, NORMAL_PRINTLN);
 
     // 10x ---> 123456789
     // 16x ---> 0xffff
@@ -91,32 +98,32 @@ test "1.2 @(1/3) ---> basic types (numbers value)" {
 
     const value_0: u32 = (3 + 2 - 1) * 3; // 12
     const x: f64 = @divTrunc(@as(i32, value_0) * (-1), 5);
-    println_fn("x", x); // -12/5 ---> -2.4 ---> -2
+    println_fn("x", x, NORMAL_PRINTLN); // -12/5 ---> -2.4 ---> -2
     const y: f64 = @divFloor(@as(i32, value_0) * (-1), 5);
-    println_fn("y", y); // -12/5 ---> -2.4 ---> -3
+    println_fn("y", y, NORMAL_PRINTLN); // -12/5 ---> -2.4 ---> -3
     const z: f64 = @divExact(value_0, 4);
-    println_fn("z", z);
+    println_fn("z", z, NORMAL_PRINTLN);
 
     const u32_max: u32 = std.math.maxInt(u32);
     assert(u32_max == 4294967295);
     const add_overflow = @addWithOverflow(u32_max, 128); // show what the bit are
-    println_fn("add_overflow", add_overflow);
+    println_fn("add_overflow", add_overflow, NORMAL_PRINTLN);
 
     const i32_min: i32 = std.math.minInt(i32);
     assert(i32_min == -2147483648);
     const min_overflow = @subWithOverflow(i32_min, 1); // 2147483647
-    println_fn("min_overflow", min_overflow);
+    println_fn("min_overflow", min_overflow, NORMAL_PRINTLN);
 
     const mul_val_0: u32 = @divTrunc(std.math.maxInt(u32), 3) + 1;
     const mul_overflow = @mulWithOverflow(mul_val_0, 3);
     assert(mul_overflow[0] == 2);
-    println_fn("mul_overflow", mul_overflow);
+    println_fn("mul_overflow", mul_overflow, NORMAL_PRINTLN);
 
     const u64_0: u64 = 1; // 0001
     const shift_amt_0: u4 = 2;
     const shl_overflow = @shlWithOverflow(u64_0, shift_amt_0);
     assert(shl_overflow[0] == 0b100);
-    println_fn("shift_amt_0", shl_overflow);
+    println_fn("shift_amt_0", shl_overflow, NORMAL_PRINTLN);
 
     var zero_val: u32 = std.math.maxInt(u32);
     zero_val +%= 3;
@@ -126,14 +133,14 @@ test "1.2 @(1/3) ---> basic types (numbers value)" {
     zero_val *%= 10;
     zero_val -%= (std.math.maxInt(u32) - 10);
     assert(zero_val == 1);
-    println_fn("zero_val", zero_val);
+    println_fn("zero_val", zero_val, NORMAL_PRINTLN);
 
     const inf_0: f64 = std.math.inf(f64);
-    println_fn("inf_0", inf_0);
+    println_fn("inf_0", inf_0, NORMAL_PRINTLN);
     const negative_inf_0: f32 = -std.math.inf(f32);
-    println_fn("negative_inf_0", negative_inf_0);
+    println_fn("negative_inf_0", negative_inf_0, NORMAL_PRINTLN);
     const nan_0: f128 = std.math.nan(f128);
-    println_fn("nan_0", nan_0);
+    println_fn("nan_0", nan_0, NORMAL_PRINTLN);
 
     assert(0.1 + 0.2 != 0.3);
 
@@ -154,23 +161,23 @@ test "1.2 @(1/3) ---> basic types (numbers value)" {
     const complex_i = Complex.init(0, 1); // Z = 0 + 1i;
 
     const z_1 = complex_i.mul(complex_i); // Z^2 = -1 + 0i
-    println_fn("z_1", z_1);
+    println_fn("z_1", z_1, NORMAL_PRINTLN);
 
     const z_2 = std.math.complex.pow(complex_i, Complex.init(2, 0));
-    println_fn("z_2", z_2);
+    println_fn("z_2", z_2, NORMAL_PRINTLN);
 
     const z_3 = std.math.complex.exp(complex_i.mul(Complex.init(std.math.pi, 0)));
-    println_fn("z_3", z_3);
+    println_fn("z_3", z_3, NORMAL_PRINTLN);
 
     const z_4 = Complex.init(1, 2).mul(Complex.init(1, -2));
-    println_fn("z_4", z_4);
+    println_fn("z_4", z_4, NORMAL_PRINTLN);
 }
 
 test "1.2 @(2/3) ---> basic types (string value)" {
     const me_zh: comptime_int = '我';
-    println_fn("me_zh", me_zh);
+    println_fn("me_zh", me_zh, NORMAL_PRINTLN);
     const me_eng: comptime_int = 'I';
-    println_fn("me_eng", me_eng);
+    println_fn("me_eng", me_eng, NORMAL_PRINTLN);
 
     const string_0: []const u8 = "Here is the zig program language";
     print("{any}\n", .{string_0});
@@ -192,16 +199,16 @@ test "1.2 @(2/3) ---> basic types (string value)" {
 
 test "1.2 @(3/3) ---> basic types (function)" {
     const add_fn = struct {
-        inline fn com_add_or_min_2b(comptime T: type, a: T, b: T) T {
+        fn com_add_or_min_2b(comptime T: type, a: T, b: T) T {
             const bm2 = 2 *| b;
             return if (a > b) @subWithOverflow(a, bm2)[0] else (a +| bm2);
         }
     };
     const val = add_fn.com_add_or_min_2b(u16, 150, 10);
-    println_fn("val", val);
+    println_fn("val", val, NORMAL_PRINTLN);
 
     const closure_func = struct {
-        inline fn football(comptime x: usize) fn (usize) usize {
+        fn football(comptime x: usize) fn (usize) usize {
             return struct {
                 fn ball_counts(y: usize) usize {
                     var counts: usize = 0;
@@ -215,7 +222,7 @@ test "1.2 @(3/3) ---> basic types (function)" {
             // return add_closure.ball_counts;
         }
 
-        inline fn baseball(x: anytype) @TypeOf(x) {
+        fn baseball(x: anytype) @TypeOf(x) {
             return x + 11;
         }
 
@@ -236,16 +243,16 @@ test "1.2 @(3/3) ---> basic types (function)" {
     _ = val_2;
 
     const val_3 = closure_func.baseball(49);
-    println_fn("val_3", val_3);
+    println_fn("val_3", val_3, NORMAL_PRINTLN);
 
     // noreturn is also a type
     // break continue return unreachable while(true) {}
 
     const val_4 = closure_func.sub_i8_mul_2_fn(5, 10);
-    println_fn("val_4", val_4);
+    println_fn("val_4", val_4, NORMAL_PRINTLN);
 
     const val_5 = closure_func.atan2(0.33, 0.55);
-    println_fn("val_5", val_5);
+    println_fn("val_5", val_5, NORMAL_PRINTLN);
 
     // @branchHint(.cold);
     // 告诉优化器当前函数很少被调用（或不被调用）。该函数仅在_函数作用域_内有效。
@@ -279,15 +286,15 @@ test "1.3 (1/8) advance type: array" {
 
     // 哨兵数组（标记终止值的数组）
     const array_with_sign: [5:7]u8 = [_:7]u8{ 1, 2, 4, 8, 16 };
-    println_fn("arr_len", array_with_sign.len);
+    println_fn("arr_len", array_with_sign.len, NORMAL_PRINTLN);
 
     // 数组重复赋值
     const array_5_times_copy: [30:7]u8 = array_with_sign ** 6; // must be [30:7] or
     // note: array sentinel '7' cannot cast into array sentinel '17'
 
     print("arr_5_copy ---> {any}\n", .{array_5_times_copy});
-    println_fn("[arr_with_sign's last] ---> ", array_with_sign[5]);
-    println_fn("[arr_5_copy's last] ---> ", array_5_times_copy[30]);
+    println_fn("[arr_with_sign's last] ---> ", array_with_sign[5], NORMAL_PRINTLN);
+    println_fn("[arr_5_copy's last] ---> ", array_5_times_copy[30], NORMAL_PRINTLN);
 
     const array_part_1: [5]isize = [_]isize{ 2, 4, 6, 8, 10 };
     const array_part_2: [5]isize = [_]isize{ 3, 5, 7, 9, 11 };
@@ -327,7 +334,7 @@ test "1.3 (2/8) advance type: vector" {
     // reduce支持
     // .And .Or .Xor
     // .Max .Min .Add .Mul
-    println_fn("rxt ---> {}\n", .{rxt});
+    println_fn("rxt ---> {}\n", .{rxt}, NORMAL_PRINTLN);
 
     const vec_y1 = @Vector(9, u8){ 'h', 'O', 'W', 'A', 'r', 'e', 'y', 'o', 'u' };
     const vec_y2 = @Vector(4, u8){ 'n', 'I', 'c', 'e' };
@@ -345,9 +352,9 @@ test "1.3 (2/8) advance type: vector" {
 test "1.3 (3/8) advance type: pointer" {
     var val_i8_0: i8 = 0b0111_1111;
     const ptr_0: *i8 = &val_i8_0;
-    println_fn("ptr_0.*", ptr_0.*);
+    println_fn("ptr_0.*", ptr_0.*, NORMAL_PRINTLN);
     ptr_0.* = -128;
-    println_fn("val_i8_0", val_i8_0);
+    println_fn("val_i8_0", val_i8_0, NORMAL_PRINTLN);
 
     var vec_0: @Vector(16, u32) = @splat(16);
     const ptr_1: *@Vector(16, u32) = &vec_0;
@@ -363,20 +370,20 @@ test "1.3 (3/8) advance type: pointer" {
     const addr = @intFromPtr(ptr_3);
     if (@TypeOf(addr) == usize and addr == 0xdeadbee0) {
         print("addr ---> HEX: {x}\n", .{addr});
-        println_fn("Successed ptr 0xdeadbee0", addr);
+        println_fn("Successed ptr 0xdeadbee0", addr, NORMAL_PRINTLN);
     }
 
     const bytes_0 align(@alignOf(u32)) = [_]u8{ 0x13, 0x12, 0x12, 0x12 };
     const u32_ptr_0: *const u32 = @ptrCast(&bytes_0);
     if (u32_ptr_0.* == 0x12121213) {
-        println_fn("u32_ptr_0", u32_ptr_0);
-        println_fn("u32_ptr_0.*", u32_ptr_0.*);
+        println_fn("u32_ptr_0", u32_ptr_0, NORMAL_PRINTLN);
+        println_fn("u32_ptr_0.*", u32_ptr_0.*, NORMAL_PRINTLN);
         // 1212_1212 ---> 303174162
         // 1212_1213 ---> 303174163
     }
 
     if (@as(u32, @bitCast(bytes_0)) == 0x12121213) {
-        println_fn("success", .{});
+        println_fn("success", .{}, NORMAL_PRINTLN);
     }
 
     // volatile
@@ -417,4 +424,47 @@ test "1.3 (3/8) advance type: pointer" {
     }
 }
 
-test "1.3 (4/8) advance type: slice" {}
+test "1.3 (4/8) advance type: slice" {
+    var array_0 = [_]i32{ 1, 2, 3, 4, 5 };
+    const slice_0 = array_0[0..3];
+    println_fn("slice_0", slice_0, ANY_PRINTLN);
+
+    for (slice_0, 1..) |elem, count| {
+        const temp = @as(usize, @intCast(@abs(-elem)));
+        println_fn("ele, + count", temp + count, NORMAL_PRINTLN);
+    }
+}
+
+test "1.3 (5/8) advance type: string" {
+    const string_0 = "banana";
+    println_fn("string_0", string_0, ANY_PRINTLN);
+
+    const mem_compare = std.mem.eql;
+    const mem_0 = mem_compare(u8, "hello", "h\x65llo");
+    println_fn("compare hello with h\\x65llo", mem_0, NORMAL_PRINTLN);
+}
+
+test "1.3 (6/8) advance type: struct" {
+    const PI = std.math.pi;
+    const Circle_OOP = struct {
+        radius: f64,
+    };
+
+    const OOP_FUNC = struct {
+        fn new(radius: f64) Circle_OOP {
+            return Circle_OOP{ .radius = radius };
+        }
+
+        fn area(self: *Circle_OOP) f64 {
+            return std.math.pow(f64, self.radius, 2.0) * PI;
+        }
+
+        fn double_the_area(self: *Circle_OOP) void {
+            self.radius *= std.math.sqrt2;
+        }
+    };
+
+    var circle_1 = OOP_FUNC.new(3.0);
+    OOP_FUNC.double_the_area(&circle_1);
+    println_fn("circle_0.area()", OOP_FUNC.area(&circle_1), NORMAL_PRINTLN);
+}
