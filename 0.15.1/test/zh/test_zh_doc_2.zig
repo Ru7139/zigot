d9ee8165eb5d6670b6df38267364ca5443e63470
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-09-22 16:01:25 +0900

    Update test_zh_doc_2.zig

--- 0.15.1/test/zh/test_zh_doc_2.zig
+++ 0.15.1/test/zh/test_zh_doc_2.zig
@@ -1,4 +1,24 @@
-test "2.1 type convert" {}
+const std = @import("std");
+const expect = std.testing.expect;
+const assert = std.debug.assert;
+const print = std.debug.print;
+
+test "2.1 type convert" {
+    // 当无法安全得从一个类型转换到另一个类型时
+    // 比如 i32 -> u32 -> u8
+    // 编译器会自动完成转换
+
+    // (comptime_float / comptime_int)
+    // is illegal
+
+    var f: f32 = undefined;
+
+    f = 32 / 5;
+    assert(f == 6);
+
+    f = 32.0 / 5.0;
+    assert(f == 6.4);
+}
 
 test "2.2 memory management" {}
 
