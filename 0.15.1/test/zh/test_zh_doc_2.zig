b577b9d461528098216f793fd0ec8021e638006b
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-09-23 23:07:30 +0900

    Update test_zh_doc_2.zig

--- 0.15.1/test/zh/test_zh_doc_2.zig
+++ 0.15.1/test/zh/test_zh_doc_2.zig
@@ -2,6 +2,15 @@ const std = @import("std");
 const expect = std.testing.expect;
 const assert = std.debug.assert;
 const print = std.debug.print;
+const mem_euqal = std.mem.eql;
+
+fn assert_u8str_eql(str_x: []const u8, str_y: []const u8) void {
+    assert(mem_euqal(u8, str_x, str_y));
+}
+
+fn display_it_print_ln(x: anytype) void {
+    print("{}\n", .{x});
+}
 
 test "2.1 type convert" {
     // 当无法安全得从一个类型转换到另一个类型时
@@ -12,12 +21,42 @@ test "2.1 type convert" {
     // is illegal
 
     var f: f32 = undefined;
+    f = 32 / 5; // 6
+    assert(std.math.approxEqAbs(f32, (32.0 / 5.0), f + 0.4, 1e-10));
 
-    f = 32 / 5;
-    assert(f == 6);
+    // 两者在 [5]u8 情况下几乎没区别
+    // 差别主要在 是否考虑结尾 0 和 写法上的显式/隐式
+    var buf_0: [5]u8 = "hello".*; // 没有0作为结尾
+    const ptr_0: [*]u8 = &buf_0;
+    ptr_0[0] = 'X';
+    assert_u8str_eql("Xello", &buf_0);
 
-    f = 32.0 / 5.0;
-    assert(f == 6.4);
+    var buf_1: [3]u8 = "yes".*;
+    const ptr_1: ?[*]u8 = &buf_1; // Option<T>
+    ptr_1.?[0] = 'K';
+    assert_u8str_eql("Kes", &buf_1);
+
+    var buf_2: [4]u8 = "ACBD".*;
+    const ptr_2: anyerror!?[*]u8 = &buf_2; // Result<Option<T>, E>
+    (try ptr_2).?[3] = 'Z';
+    assert_u8str_eql("ACBZ", &buf_2);
+
+    // 联合枚举 union(enum) 可自行推断
+
+    var buf_3: i32 = 1234;
+    const ptr_3: *[1]i32 = &buf_3; // 先转化为array
+    const ptr_3_b: [*]i32 = ptr_3; // 转化到array ptr
+    ptr_3_b[0] = 5;
+    assert(buf_3 == 5);
+
+    const buf_4_a: i32 = -125;
+    const buf_4_b: u32 = @bitCast(buf_4_a);
+    assert(buf_4_b == 4294967171);
+
+    var buf_5_a: u32 = undefined;
+
+    const buf_5_b: u32 = @alignCast(buf_5_a);
+    display_it_print_ln(buf_5_b);
 }
 
 test "2.2 memory management" {}
